d24bddec73bd6005c0b3c8bee16a1af38990f133
Dung Doan <harrypeterb2@gmail.com>
2022-07-04 11:04:15 +0700

    computer_v2

--- face_detect.py
+++ face_detect.py
@@ -4,38 +4,42 @@ import torch
 import numpy as np
 import time
 
-device =  torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print(device)
 prev_frame_time = 0
 new_frame_time = 0
 
-mtcnn = MTCNN(thresholds= [0.7, 0.7, 0.8] ,keep_all=True, device = device)
+mtcnn = MTCNN(thresholds=[0.7, 0.7, 0.8], keep_all=True, device=device)
 
-cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH,640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT,480)
+cap = cv2.VideoCapture(
+    r"rtsp://admin:space123@192.168.1.47:554/cam/realmonitor?channel=1&subtype=1")
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 while cap.isOpened():
     isSuccess, frame = cap.read()
     if isSuccess:
         boxes, _, points_list = mtcnn.detect(frame, landmarks=True)
         if boxes is not None:
             for box in boxes:
-                bbox = list(map(int,box.tolist()))
-                frame = cv2.rectangle(frame,(bbox[0],bbox[1]),(bbox[2],bbox[3]),(0,0,255),6)
+                bbox = list(map(int, box.tolist()))
+                frame = cv2.rectangle(
+                    frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0, 0, 255), 2)
                 # Bo comment phan duoi neu cac ban muon xem 5-points tren mat
 
                 # if(not isinstance(points_list, list)):
-                #     points_list= points_list.tolist()
+                #     points_list = points_list.tolist()
                 # for usr in points_list:
                 #     for points in usr:
-                #         frame = cv2.circle(frame, (int(points[0]), int(points[1])), radius=0, color=(0,0,255), thickness=10)
+                #         frame = cv2.circle(frame, (int(points[0]), int(
+                #             points[1])), radius=0, color=(0, 0, 255), thickness=10)
     new_frame_time = time.time()
-    fps = 1/(new_frame_time-prev_frame_time)
+    fps = 20/(new_frame_time-prev_frame_time)
     prev_frame_time = new_frame_time
     fps = str(int(fps))
-    cv2.putText(frame, fps, (7, 70), cv2.FONT_HERSHEY_DUPLEX, 3, (100, 255, 0), 3, cv2.LINE_AA)
+    cv2.putText(frame, fps, (20, 70), cv2.FONT_HERSHEY_DUPLEX,
+                1, (100, 255, 0), 1, cv2.LINE_AA)
     cv2.imshow('Face Detection', frame)
-    if cv2.waitKey(1)&0xFF == 27:
+    if cv2.waitKey(1) & 0xFF == 27:
         break
 cap.release()
 cv2.destroyAllWindows()
--- face_recognition.py
+++ face_recognition.py
@@ -6,16 +6,19 @@ import numpy as np
 from PIL import Image
 import time
 
-frame_size = (640,480)
+
+frame_size = (640, 480)
 IMG_PATH = './data/test_images'
 DATA_PATH = './data'
 
+
 def trans(img):
     transform = transforms.Compose([
-            transforms.ToTensor(),
-            fixed_image_standardization
-        ])
+        transforms.ToTensor(),
+        fixed_image_standardization
+    ])
     return transform(img)
+
 
 def load_faceslist():
     if device == 'cpu':
@@ -25,19 +28,22 @@ def load_faceslist():
     names = np.load(DATA_PATH+'/usernames.npy')
     return embeds, names
 
-def inference(model, face, local_embeds, threshold = 3):
-    #local: [n,512] voi n la so nguoi trong faceslist
+
+def inference(model, face, local_embeds, threshold=3):
+    # local: [n,512] voi n la so nguoi trong faceslist
     embeds = []
     # print(trans(face).unsqueeze(0).shape)
     embeds.append(model(trans(face).to(device).unsqueeze(0)))
-    detect_embeds = torch.cat(embeds) #[1,512]
+    detect_embeds = torch.cat(embeds)  # [1,512]
     # print(detect_embeds.shape)
-                    #[1,512,1]                                      [1,512,n]
-    norm_diff = detect_embeds.unsqueeze(-1) - torch.transpose(local_embeds, 0, 1).unsqueeze(0)
+    #[1,512,1]                                      [1,512,n]
+    norm_diff = detect_embeds.unsqueeze(-1) - \
+        torch.transpose(local_embeds, 0, 1).unsqueeze(0)
     # print(norm_diff)
-    norm_score = torch.sum(torch.pow(norm_diff, 2), dim=1) #(1,n), moi cot la tong khoang cach euclide so vs embed moi
-    
-    min_dist, embed_idx = torch.min(norm_score, dim = 1)
+    # (1,n), moi cot la tong khoang cach euclide so vs embed moi
+    norm_score = torch.sum(torch.pow(norm_diff, 2), dim=1)
+
+    min_dist, embed_idx = torch.min(norm_score, dim=1)
     print(min_dist*power, names[embed_idx])
     # print(min_dist.shape)
     if min_dist*power > threshold:
@@ -45,13 +51,14 @@ def inference(model, face, local_embeds, threshold = 3):
     else:
         return embed_idx, min_dist.double()
 
+
 def extract_face(box, img, margin=20):
     face_size = 160
     img_size = frame_size
     margin = [
         margin * (box[2] - box[0]) / (face_size - margin),
         margin * (box[3] - box[1]) / (face_size - margin),
-    ] #tạo margin bao quanh box cũ
+    ]  # tạo margin bao quanh box cũ
     box = [
         int(max(box[0] - margin[0] / 2, 0)),
         int(max(box[1] - margin[1] / 2, 0)),
@@ -59,9 +66,11 @@ def extract_face(box, img, margin=20):
         int(min(box[3] + margin[1] / 2, img_size[1])),
     ]
     img = img[box[1]:box[3], box[0]:box[2]]
-    face = cv2.resize(img,(face_size, face_size), interpolation=cv2.INTER_AREA)
+    face = cv2.resize(img, (face_size, face_size),
+                      interpolation=cv2.INTER_AREA)
     face = Image.fromarray(face)
     return face
+
 
 if __name__ == "__main__":
     prev_frame_time = 0
@@ -76,11 +85,12 @@ if __name__ == "__main__":
     ).to(device)
     model.eval()
 
-    mtcnn = MTCNN(thresholds= [0.7, 0.7, 0.8] ,keep_all=True, device = device)
+    mtcnn = MTCNN(thresholds=[0.7, 0.7, 0.8], keep_all=True, device=device)
 
-    cap = cv2.VideoCapture(0)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH,640)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT,480)
+    cap = cv2.VideoCapture(
+        r"rtsp://admin:space123@192.168.1.47:554/cam/realmonitor?channel=1&subtype=1")
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
     embeddings, names = load_faceslist()
     while cap.isOpened():
         isSuccess, frame = cap.read()
@@ -88,25 +98,31 @@ if __name__ == "__main__":
             boxes, _ = mtcnn.detect(frame)
             if boxes is not None:
                 for box in boxes:
-                    bbox = list(map(int,box.tolist()))
+                    bbox = list(map(int, box.tolist()))
                     face = extract_face(bbox, frame)
                     idx, score = inference(model, face, embeddings)
                     if idx != -1:
-                        frame = cv2.rectangle(frame, (bbox[0],bbox[1]), (bbox[2],bbox[3]), (0,0,255), 6)
-                        score = torch.Tensor.cpu(score[0]).detach().numpy()*power
-                        frame = cv2.putText(frame, names[idx] + '_{:.2f}'.format(score), (bbox[0],bbox[1]), cv2.FONT_HERSHEY_DUPLEX, 2, (0,255,0), 2, cv2.LINE_8)
+                        frame = cv2.rectangle(
+                            frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0, 255, 0), 2)
+                        score = torch.Tensor.cpu(
+                            score[0]).detach().numpy()*power
+                        frame = cv2.putText(frame, names[idx] + '_{:.1f}'.format(
+                            score), (bbox[0], bbox[1]), cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 0), 2, cv2.LINE_8)
                     else:
-                        frame = cv2.rectangle(frame, (bbox[0],bbox[1]), (bbox[2],bbox[3]), (0,0,255), 6)
-                        frame = cv2.putText(frame,'Unknown', (bbox[0],bbox[1]), cv2.FONT_HERSHEY_DUPLEX, 2, (0,255,0), 2, cv2.LINE_8)
+                        frame = cv2.rectangle(
+                            frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0, 255, 0), 2)
+                        frame = cv2.putText(
+                            frame, 'Unknown', (bbox[0], bbox[1]), cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 0), 2, cv2.LINE_8)
 
             new_frame_time = time.time()
             fps = 1/(new_frame_time-prev_frame_time)
             prev_frame_time = new_frame_time
             fps = str(int(fps))
-            cv2.putText(frame, fps, (7, 70), cv2.FONT_HERSHEY_DUPLEX, 3, (100, 255, 0), 3, cv2.LINE_AA)
+            cv2.putText(frame, fps, (20, 70), cv2.FONT_HERSHEY_DUPLEX,
+                        1, (255, 215, 0), 1, cv2.LINE_AA)
 
         cv2.imshow('Face Recognition', frame)
-        if cv2.waitKey(1)&0xFF == 27:
+        if cv2.waitKey(1) & 0xFF == 27:
             break
 
     cap.release()
